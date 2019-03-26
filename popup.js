ead8e15d59230c24532d2db7781cb2ae35e2e521
MudSais <mudsais@gmail.com>
2019-03-26 23:27:08 +0100

    Commit du mercredi

---
+++ popup.js
@@ -0,0 +1,31 @@
+/*
+
+var theguest= setTimeout("stay_here()",1000);
+
+function stay_here() {
+
+    if (!window.confirm("Do you want to continue using this web page?")) {
+        window.open("http://www.google.com","Hey, it works.","width=600,height=600");
+    }
+     else {
+        alert("OK. My advertising customers will be happy!");
+        return false;
+    }
+}
+
+*/
+
+var LeTemps = setTimeout("Milliards_de_dollars_zimbabwéen()",10000);
+
+function Milliards_de_dollars_zimbabwéen () {
+
+    if (window.confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please'))
+    {
+        alert('Have a nice day -_^');
+        window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    }
+    else
+    {
+        die();
+    }
+}
